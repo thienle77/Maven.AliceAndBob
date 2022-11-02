f81bf32b18e499f87153d1c2183cd77fa501175f
thienle77 <tle9677@gmail.com>
2022-11-01 21:53:51 -0400

    Completed lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,27 @@
+
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello what is your name?");
+        Scanner scanner = new Scanner (System.in);
+        String s = scanner.nextLine();
+        System.out.println("Your name is " + s);
 
-    public static void main(String[] args ){
+
+        if (s .equals("Bob") || s .equals ("Alice")) {
+            System.out.println("Greetings " + s) ;
+        } else {
+            System.out.println("Sorry, name was not found");
+        }
 
     }
 }
+
+
+
+
+
